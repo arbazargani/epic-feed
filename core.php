a92158a37f07f941e092701cca5ef8147ccfcc2a
علی رضا بازرگانی <a.bazargani@smt.news>
2020-12-27 15:24:36 +0330

    Ui, Jdate.

--- core.php
+++ core.php
@@ -1,6 +1,5 @@
 <?php
-
-echo "<pre>";
+require 'jdf.php';
 
 function XMLrender($streams) {
     $data = [];
@@ -40,9 +39,59 @@ function XMLrender($streams) {
     return $timestamps;
 };
 
+function FileXMLrender ($streams) {
+    header('Content-type: text/html; charset=utf-8');
+    mb_internal_encoding('UTF-8');
+
+    $data = [];
+    $timestamps = [];
+    foreach($streams as $owner => $stream) {
+        $stream = file_get_contents($stream);
+        $handle= fopen("stream.txt", "w");
+        fwrite($handle, $stream);
+
+        $parser = simplexml_load_file($stream);
+        
+        foreach ($parser->channel->item as $item) {
+            $date = (string )$item->pubDate;
+            // echo "$date\r\n";
+            $time = strtotime($item->pubDate);
+            // echo "$time\r\n";
+            $link  = (string) $item->link;
+            // echo "$link\r\n";
+            $title = (string) $item->title;
+            // echo "$title\r\n";
+            $text  = (string) $item->description;
+            // echo "<hr>";
+        
+            $data[] = [
+                'src' => $owner,
+                'timestamp' => $time,
+                'url' => $link,
+                'title' => $title
+            ];
+        
+            $timestamps[$time] = [
+                'src' => $owner,
+                'timestamp' => $time,
+                'url' => $link,
+                'title' => $title
+            ];
+        }
+    }
+}
+
 $op = XMLrender([
-    'gostaresh.news' => 'https://www.gostaresh.news/fa/feeds/?p=Y2F0ZWdvcmllcz04Mw%2C%2C',
-    'rokna.net' => 'https://www.rokna.net/fa/feeds/?p=Y2F0ZWdvcmllcz02NQ%2C%2C'
+    'گسترش‌نیوز' => 'https://www.gostaresh.news/fa/feeds/?p=Y2F0ZWdvcmllcz04Mw%2C%2C',
+    'دنیای اقتصاد' => 'https://donya-e-eqtesad.com/feeds/',
+    'افکار نیوز' => 'https://www.afkarnews.com/fa/feeds',
+    'برنا' => 'https://www.borna.news/fa/feeds'
 ]);
 
-print_r($op);
+// $op = FileXMLrender([
+//     // 'gostaresh.news' => 'https://www.gostaresh.news/fa/feeds/?p=Y2F0ZWdvcmllcz04Mw%2C%2C',
+//     // 'rokna.net' => 'https://www.rokna.net/fa/feeds/?p=Y2F0ZWdvcmllcz02NQ%2C%2C',
+//     // 'yjc.ir' => 'https://www.yjc.ir/fa/rss/5/18'
+// ]);
+// echo '<pre>';
+// print_r($op);
--- kit.php
+++ kit.php
@@ -12,37 +12,12 @@
         <script src="assets/js/uikit-icons.min.js"></script>
         <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
         <style>
-            @font-face {
-                font-family: "Vazir";
-                src: url("assets/fonts/Vazir-Light.eot");
-                src: url("assets/fonts/Vazir-Light?#iefix") format("embedded-opentype"), url("assets/fonts/Vazir-Light.woff") format("woff"), url("assets/fonts/Vazir-Light.ttf") format("truetype");
-                font-weight: normal;
-            }
-            @font-face {
-                font-family: "Vazir-bold";
-                src: url("assets/fonts/Vazir-Bold.eot");
-                src: url("assets/fonts/Vazir-Bold.eot?#iefix") format("embedded-opentype"), url("assets/fonts/Vazir-Bold.ttf") format("truetype");
-                font-weight: bold;
-            }
-            @font-face {
-                font-family: "Vazir-medium";
-                src: url("assets/fonts/Vazir-Medium.eot");
-                src: url("assets/fonts/Vazir-Medium.eot?#iefix") format("embedded-opentype"), url("assets/fonts/Vazir-Medium.ttf") format("truetype");
-                font-weight: 200;
-            }
-            * {
-                font-family: 'Vazir';
-                color: #2c3e50;
-            }
             body {
                 /* background: url('https://www.teahub.io/photos/full/111-1110876_spotify-wallpaper.jpg');
                 background-repeat: no-repeat;
                 background-position: center; */
                 background: #34495e;
                 min-height: 100vh;
-            }
-            table tbody tr td {
-                padding: 3px !important;
             }
             #root-container {
                 
@@ -61,12 +36,12 @@
         </style>
     </head>
     <body>
-        <div id="root-container" class="uk-container uk-container-medium uk-margin-auto uk-padding-large">
+        <div id="root-container" class="uk-container uk-container-small uk-margin-auto uk-padding-large">
             <div class="uk-card uk-card-default uk-card-large uk-card-body uk-width-1-1@m uk-border-rounded opacity-90">
                 <ul uk-tab>
-                    <li><a class="uk-text-bold" href="#">اقتصادی</a></li>
-                    <li><a class="uk-text-bold" href="#">سیاسی</a></li>
-                    <li><a class="uk-text-bold" href="#">حوادث</a></li>
+                    <li><a href="#">اقتصادی</a></li>
+                    <li><a href="#">سیاسی</a></li>
+                    <li><a href="#">حوادث</a></li>
                 </ul>
 
                 <ul class="uk-switcher uk-margin">
@@ -92,31 +67,12 @@
                             </tfoot>
                             -->
                             <tbody>
-                            <?php
-                                $counter = 0;
-                                foreach ($op as $timestamp => $item):
-                                if (++$counter == 26) break;
-                            ?>
-                            <?php
-                                
-                                $timestamp_to_date = date('Y/d/m H:i:s', $item['timestamp']);
-                                $date_part = explode(' ', $timestamp_to_date);
-                                $date_part = explode('/', $date_part[0]);
- 
-                                $jYear  = $date_part[0];
-                                $jMonth = $date_part[1];
-                                $jDay   = $date_part[2];
-
-                                $date = gregorian_to_jalali($jYear, $jMonth, $jDay, '-');
-                                $en = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-                                $fa = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-                                $date = str_replace($en, $fa, $date);
-                            ?>
+                            <?php foreach ($op as $timestamp => $item): ?>
                                 <tr>
                                     <td class="uk-table-shrink"><ion-icon name="ellipse-outline" class="blink"></ion-icon></td>
                                     <td class="uk-table-expand"><?php echo $item['title']; ?></td>
-                                    <td class="uk-table-expand"><?php echo $item['src']; ?></td>
-                                    <td class="uk-table-expand"><?php echo $date; ?></td>
+                                    <td class="uk-table-small"><?php echo $item['src']; ?></td>
+                                    <td class="uk-table-small"><?php echo date('Y/d/m H:i:s', $item['timestamp']); ?></td>
                                 </tr>
                             <?php endforeach; ?>
                             </tbody>
